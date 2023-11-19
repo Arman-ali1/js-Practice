189173cf66cbb6e5351a588c516a284483fd064a
Arman Ali <armnsrn173200@gmail.com>
2023-11-20 02:28:48 +0530

    Initial commit

--- State-control/src/App.jsx
+++ State-control/src/App.jsx
@@ -2,9 +2,10 @@
 import Count from './components/Count'
 import Card from './components/Card'
 function App() {
+ 
   return (
     <>
-      <div flex justify-center items-center flex-col>
+      <div className="flex justify-center items-center flex-col">
         <div className="flex justify-center items-center flex-row">
             <Card name="asdf"/>
             <Card name="Arman"/>
--- State-control/src/components/Count.jsx
+++ State-control/src/components/Count.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 function Count(){
-    const [Count, setCount] = useState(0);
-    function counter(){
-        setCount(Count+1);
+    const [Counter, setCount] = useState(0);
+    function addValue(){
+        if(Counter<=20)
+        setCount(Counter+1);
+        else{
+            
+            alert(" OOH oo! You can't add more than 20");
+
+        }
     }
     return(
-        <div>
-            <h1>(: This is a counter :)</h1>
-            <button onClick={counter} className="cursor-pointer">{Count} :)</button>
+        <div className="flex justify-center items-center flex-col">
+            <h1 className="text-fuchsia-400 font-serif text-8xl text-center " >(: This is a counter {Counter} :)</h1>
+            <button onClick={addValue} className="cursor-pointer text-red-600 text-3xl font-bold ">Add {Counter} :)</button>
         </div>
     );
 }
